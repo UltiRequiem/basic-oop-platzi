de601a18b575a6fcc1eda1372a6b276a731fde5f
Zer <eliaz.bobadilladev@gmail.com>
2021-01-28 21:09:47 -0500

    JavaScript

---
+++ Desarrollo/JS/Uber/UberX.js
@@ -0,0 +1,6 @@
+function UberX(model, brand) {
+    this.model = model
+    this.door = 4
+    this.brand = brand
+    this.passengers = 4
+}
---
+++ Desarrollo/JS/Uber/index.js
@@ -0,0 +1,13 @@
+//Mostrar cómo se llama un objeto en JS 
+var uberX = new UberX();
+console.log(uberX.model);
+console.log(uberX.brand);
+console.log(uberX.door);
+console.log(uberX.passengers);
+
+//Declarar un constructor
+var uberXAveo = new UberX(2019, "Aveo");
+console.log(uberXAveo.model);
+console.log(uberXAveo.brand);
+console.log(uberXAveo.door);
+console.log(uberXAveo.passengers);
